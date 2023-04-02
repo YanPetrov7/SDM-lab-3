95d611cd55b3a37d9cc5c73a62ec5fed1e53bf89
YanPetrov7 <yanemerald2004@gmail.com>
2023-04-02 22:00:47 +0300

    add an endpoint with matrices

--- starter-project-python/spaceship/routers/api.py
+++ starter-project-python/spaceship/routers/api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import numpy as numpy
 
 router = APIRouter()
 
@@ -12,3 +13,16 @@ def hello_world() -> dict:
         'height': '190',
         'weight': '80'
         }
+
+@router.get('/matrix')
+def matrix() -> dict:
+    matrix_a = numpy.random.rand(10, 10)
+    matrix_b = numpy.random.rand(10, 10)
+    
+    product = numpy.dot(matrix_a, matrix_b)
+
+    return {
+        "matrix A": matrix_a.tolist(),
+        "matrix B": matrix_b.tolist(),
+        "result": product.tolist()
+    }
